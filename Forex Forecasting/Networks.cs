421c0f611f62c84e1eb59132706348c66f2bddd2
Nikita6000 <nik96a@gmail.com>
2017-04-28 15:46:43 +0300

    Added NeuralNetworkCore

--- Forex Forecasting/Networks.cs
+++ Forex Forecasting/Networks.cs
@@ -340,10 +340,53 @@ namespace Forex_Forecasting
         }
     }
 
-    public class NeuralNetworkCore
+    // Parent class for a range of neural network
+    public class NeuralNetworkCore<DataType>
     {
-        private List<Node<double>> Nodes = new List<Node<double>>();
+        private static int ID;
 
+        protected List<Node<DataType>> _Nodes = new List<Node<DataType>>();
+        protected int _NetworkID;
 
+        public List<Node<DataType>> Nodes
+        {
+            get { return _Nodes; }
+            set { _Nodes = value; }
+        }
+
+        public int NetworkID
+        {
+            get { return _NetworkID; }
+            set { _NetworkID = value; }
+        }
+
+        // static constructor, sets ID counter for all inherited networks
+        static NeuralNetworkCore()
+        {
+            ID = 0;
+        }
+
+        // public constructor, sets ID for this network
+        public NeuralNetworkCore()
+        {
+            _NetworkID = GetNewID();
+        }
+
+        // Add new empty node to network
+        protected void AddNode()
+        {
+            _Nodes.Add(new Node<DataType>());
+        }
+
+        // Add node to network
+        protected void AddNode(Node<DataType> NewNode)
+        {
+            _Nodes.Add(NewNode);
+        }
+
+        private int GetNewID()
+        {
+            return ID++;
+        }
     }
 }
